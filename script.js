bf35dfbeac8dc4e02c7faf1764522dfc9b15f81d
Jose Romero <joseromero@unilene.com>
2019-12-11 16:43:22 -0500

    Utilizando metodo MAP y usando for of en nodos. Manipulando atributos. Manipulando contenido. Creando elementos en el DOM

--- script.js
+++ script.js
@@ -42,22 +42,74 @@ let c = console.log
 // const elementList = document.getElementsByTagName('li')
 // c(elementList)
 
-const nodeList = document.querySelectorAll('li')
-c(nodeList)
-// nodeList.map(el => el.style.background = 'gray')
-for (let el of nodeList){
-    if(el.textContent.trim().toUpperCase() === 'OBJECT'){
-        el.remove()
-    }
-}
-
-const nodeListToArray = [...document.querySelectorAll('li')]
-nodeListToArray.map(el => el.style.background = 'gray')
-
-const nodeListToArrayPrototype = Array.from(document.querySelectorAll('li'))
-nodeListToArrayPrototype.map(el => el.style.background = 'lightgray')
-// nodeListToArrayPrototype.map(el => {
+// const nodeList = document.querySelectorAll('li')
+// c(nodeList)
+// // nodeList.map(el => el.style.background = 'gray')
+// for (let el of nodeList){
 //     if(el.textContent.trim().toUpperCase() === 'OBJECT'){
 //         el.remove()
 //     }
-// })
+// }
+
+// const nodeListToArray = [...document.querySelectorAll('li')]
+// nodeListToArray.map(el => el.style.background = 'gray')
+
+// const nodeListToArrayPrototype = Array.from(document.querySelectorAll('li'))
+// nodeListToArrayPrototype.map(el => el.style.background = 'lightgray')
+// // nodeListToArrayPrototype.map(el => {
+// //     if(el.textContent.trim().toUpperCase() === 'OBJECT'){
+// //         el.remove()
+// //     }
+// // })
+
+
+
+
+// const title = document.getElementById('title')
+// c(title.getAttribute('id'))
+// c(title.getAttribute('class'))
+// c(title.getAttribute('value'))
+
+// const title2 = document.getElementById('title2')
+// if(title2){
+//     title2.style.background = 'yellow'
+// }
+// c('Hola mundo')
+
+// const parrafo = document.querySelector('p')
+// // parrafo.setAttribute('id','parrafo')
+// parrafo.id = 'parrafo'
+// c(parrafo)
+// c(parrafo.classList)
+// parrafo.classList.add('contenido','mainParrafo')
+// parrafo.classList.remove('contenido','mainParrafo')
+// parrafo.classList.toggle('contenido')
+// c(parrafo.id)
+
+// const campo = document.querySelector('input')
+// c(campo.value)
+
+
+
+
+// const title = document.querySelector('h1')
+// c(title.textContent)
+// c(title.innerHTML)
+// c(title.outerHTML)
+// // title.textContent = 'Hola mundo <em>cruel</em>'
+// title.innerHTML = `Hola mundo <em>cruel</em>`
+
+
+
+const usuario = document.createElement('p')
+// usuario.textContent = 'Bienvenidos Usuarios'
+usuario.innerHTML = `<em>usuarios</em>`
+usuario.classList.add('users')
+usuario.id = 'usuario'
+
+// document.body.appendChild(usuario)
+
+const containerBienvenida = document.getElementById('contenedor-bienvenida')
+if(containerBienvenida && containerBienvenida.querySelector('span')){
+    containerBienvenida.querySelector('span').appendChild(usuario)
+}
